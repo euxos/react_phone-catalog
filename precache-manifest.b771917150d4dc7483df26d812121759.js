7351a67b8246abfb94d62830f06d7af56231bb14
euxos <eugen.ostrozhinsky@gmail.com>
2020-07-16 01:03:06 +0300

    Updates

---
+++ precache-manifest.b771917150d4dc7483df26d812121759.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03095c2b062953f1129999614cde376",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27d29920e94f7e31fa93",
+    "url": "./static/css/main.f136af2f.chunk.css"
+  },
+  {
+    "revision": "a591a7bb29bf6b796331",
+    "url": "./static/js/2.5a9bbc0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.5a9bbc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d29920e94f7e31fa93",
+    "url": "./static/js/main.5145a49f.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "032a50cf1345af9ac76324ff5b0e4e2d",
+    "url": "./static/media/Cart.032a50cf.svg"
+  },
+  {
+    "revision": "ba064880a8e6bce13a241bd23737f156",
+    "url": "./static/media/Favorites.ba064880.svg"
+  },
+  {
+    "revision": "49f665877b6b6081e14cef4569870268",
+    "url": "./static/media/LOGO.49f66587.svg"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.b771917150d4dc7483df26d812121759.js"
 );
 
 self.addEventListener('message', (event) => {
